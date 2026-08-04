870c4feda4001c53419f0f594983dc75c60278c7
olawu <olawunmi.ajibola@tfgm.com>
2026-08-04 21:20:09 +0100

    Functions

--- 3.4+Birthday+Invite+Project/quiz.js
+++ 3.4+Birthday+Invite+Project/quiz.js
@@ -1,11 +1,20 @@
-var maxChars = 140;
-var sentence = "bola is a girlie";
-var count = sentence.length;
+//var maxChars = 140;
+//var sentence = "bola is a girlie";
+//var count = sentence.length;
 
 
 //console.log(sentence.length);
 //sentence = sentence.slice(2,8);
 //console.log(sentence);
 //console.log(sentence.toUpperCase())
-sentence=sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length);
-console.log(sentence);
+//sentence=sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length);
+//console.log(sentence);
+
+//How many bottles of milk can my robot buy if a bottle is £1.50
+function getMilk(amount){
+    costOfABottle = 1.5;
+    numberOfBottles = Math.floor(amount/costOfABottle);
+console.log("You can buy "+ numberOfBottles + " bottles of milk")
+}
+
+getMilk(8);
